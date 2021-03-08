35069626edbf3a9a1ad8053552b4e5a277876b91
Oleksandr Klymenko <olexandr.klymenko@gmail.com>
2021-03-08 13:36:42 +0200

    Healthcheck DONE. 1st real test

--- tests/test_cars.py
+++ tests/test_cars.py
@@ -5,4 +5,13 @@ class TestCarsApi(TestApiBase):
     def test_get_all_dealers(self):
         resp = self.get("dealers", params={"email": "info@fast_deal.com"})
         assert resp.status_code == 200
-        assert resp.json() == []
+        assert resp.json() == [
+            {
+                "location": "Houston Texas",
+                "email": "info@fast_deal.com",
+                "phone": "832-555-2002",
+                "website": "fast_deal.com",
+                "vehicles": [],
+                "id": 1,
+            }
+        ]
