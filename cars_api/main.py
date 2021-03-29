ad2c74dc007dc811817f9c8737a0740952ee55e7
oklymenko <olexandr.klymenko@gmail.com>
2021-03-29 15:15:22 +0300

    Added for debugging

--- cars_api/main.py
+++ cars_api/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from cars_api.routers.dealers import router as dealers_router
@@ -8,3 +9,9 @@ app = FastAPI()
 app.include_router(dealers_router, prefix="/dealers", tags=["dealers"])
 app.include_router(vehicles_router, prefix="/vehicles", tags=["vehicles"])
 app.include_router(info_router)
+
+if __name__ == "__main__":
+    """
+    Set up environment variable SQLALCHEMY_DATABASE_URL="sqlite:///../db/sql.db"
+    """
+    uvicorn.run(app, host="0.0.0.0", port=8000)
